280fbb3213bc52d719fc69fe615036c6387cd578
Sergio Castell Martínez <sergiocastell@gmail.com>
2020-06-13 11:56:26 +0200

    reorder git commands

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-git checkout -- . ; source .env && git pull && sudo docker build -t postmanagerapp . && sudo docker-compose down && sudo docker-compose up -d
+git checkout -- . ; git pull ; source .env && sudo docker build -t postmanagerapp . && sudo docker-compose down && sudo docker-compose up -d
